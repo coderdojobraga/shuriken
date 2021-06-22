d1022e9d86bc2cb6b883e1869fd68c603741293e
Nelson Estevão <nelsonmestevao@gmail.com>
2021-06-22 21:48:35 +0100

    Add profile projects tabs (#21)

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -14,6 +14,7 @@ import {
 import { ClockCircleOutlined, UserOutlined } from "@ant-design/icons";
 import moment from "moment";
 import Badge from "~/components/Badge";
+import Document from "~/components/Document";
 import LinkTo from "~/components/utils/LinkTo";
 import * as api from "~/lib/utils/api";
 import * as BELT from "~/lib/utils/belt";
@@ -28,18 +29,24 @@ const { Title } = Typography;
 function Profile({ id, type }) {
   const [info, setInfo] = useState({});
   const [badges, setBadges] = useState([]);
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     api
       .getUserByType({ id, type })
       .then((response) => setInfo(response.data))
-      .catch((error) => notification["error"](error.data));
+      .catch((error) => notification["error"](error.data?.errors));
 
     if (type == USER.ROLES.NINJA) {
       api
         .getNinjaBadges(id)
         .then((response) => setBadges(response.data))
-        .catch((error) => notification["error"](error.data));
+        .catch((error) => notification["error"](error.data?.errors));
+
+      api
+        .getNinjaFiles(id)
+        .then((response) => setProjects(response.data))
+        .catch((error) => notification["error"](error.data?.errors));
     }
   }, [id, type]);
 
@@ -136,7 +143,22 @@ function Profile({ id, type }) {
           </TabPane>
         )}
         <TabPane tab="Projetos" key="3">
-          Content of Tab Pane 3
+          {projects.length == 0 ? (
+            <Empty
+              description="Sem Projetos"
+              image={Empty.PRESENTED_IMAGE_SIMPLE}
+            />
+          ) : (
+            <Row justify="start" align="middle">
+              {projects.map((project) => (
+                <Col key={project.id} {...{ xs: 24, md: 12, xl: 8, xxl: 6 }}>
+                  <Space>
+                    <Document {...project} />
+                  </Space>
+                </Col>
+              ))}
+            </Row>
+          )}
         </TabPane>
       </Tabs>
     </>
