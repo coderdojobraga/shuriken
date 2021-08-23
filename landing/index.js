399996ae0c50ba2c4bbac7c1c3726fdb6031a2d5
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-08-23 21:35:18 +0100

    ✨ Add team section
    Added a team section to the landing. Note that the footer is in the
    wrong color - this fix is pending on #24.

--- landing/index.js
+++ landing/index.js
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
+import Member from "./Member";
 
-export { Footer, Header, Hero };
+export { Footer, Header, Hero, Member };
