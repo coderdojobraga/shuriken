b75ec5fb369a92e7103a0a03da79cc4172491738
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-08-27 23:25:13 +0100

    ⚡️ Add blog menu page

--- components/DarkModeToggle/index.js
+++ components/DarkModeToggle/index.js
@@ -3,20 +3,20 @@ import { useTheme } from "../Theme";
 import styles from "./style.module.css";
 
 function DarkModeToggle() {
-  const { isDark, toggleTheme } = useTheme();
-  return (
-    <button onClick={toggleTheme}>
-      {isDark ? (
-        <div className={styles.circle_dark}>
-          <div className={styles.crescent_dark}></div>
-        </div>
-      ) : (
-        <div className={styles.circle}>
-          <div className={styles.crescent}></div>
-        </div>
-      )}
-    </button>
-  );
+    const { isDark, toggleTheme } = useTheme();
+    return (
+        <button onClick={toggleTheme}>
+            {isDark ?
+                <div className={styles.circle_dark}>
+                    <div className={styles.crescent_dark}></div>
+                </div>
+                :
+                <div className={styles.circle}>
+                    <div className={styles.crescent}></div>
+                </div>
+            }
+        </button>
+    );
 }
 
 export default DarkModeToggle;
