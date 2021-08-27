39e8a3b5281e32d340bc3d21adb231139e2bc877
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-08-27 23:25:13 +0100

    ⚡️ Add blog menu page

---
+++ landing/blog/Entry/index.js
@@ -0,0 +1,41 @@
+import { useTheme } from "../../../components/Theme";
+
+export default function Entry({ title, text, author, date, photo, topic }) {
+  const { isDark } = useTheme();
+  return (
+    <div class={`${isDark ? "dark" : "light"} mt-6`}>
+      <div class="max-w-4xl px-10 py-6 dark:bg-altdark bg-white rounded-lg shadow-md">
+        <div class="flex items-center justify-between">
+          <span class="font-light dark:text-white text-gray-600">{date}</span>
+          <a href="#" class="px-2 py-1 font-bold text-white bg-primary rounded">
+            {topic}
+          </a>
+        </div>
+        <div class="mt-2">
+          <a
+            href="#"
+            class="text-2xl font-bold dark:text-white text-dark hover:text-primary hover:underline"
+          >
+            {title}
+          </a>
+          <p class="mt-2 dark:text-white text-gray-600">{text}</p>
+        </div>
+        <div class="flex items-center justify-between mt-4">
+          <a href="#" class="dark:text-white text-primary hover:underline">
+            Read more
+          </a>
+          <div>
+            <a href="#" class="flex items-center">
+              <h1 class="font-bold dark:text-white text-dark hover:underline">{author}</h1>
+              <img
+                src={`img/team/${photo}`}
+                alt="avatar"
+                class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ landing/blog/Feature/index.js
@@ -0,0 +1,29 @@
+export default function Feature({ title, author, date, photo, topic }) {
+  return (
+    <div class="dark:bg-altdark mt-4 flex flex-col max-w-sm px-8 py-6 bg-white rounded-lg shadow-md">
+      <div class="flex items-center">
+        <a href="#" class="px-2 py-1 text-sm text-white bg-primary rounded">
+          {topic}
+        </a>
+      </div>
+      <div class="mt-4">
+        <a href="#" class="text-lg font-bold dark:text-white text-dark hover:underline">
+          {title}
+        </a>
+      </div>
+      <div class="flex items-center justify-between mt-4">
+        <div class="flex items-center">
+          <img
+            src={`img/team/${photo}`}
+            alt="avatar"
+            class="object-cover w-8 h-8 rounded-full"
+          />
+          <a href="#" class="mx-3 text-sm dark:text-white text-gray-700 hover:underline">
+            {author}
+          </a>
+        </div>
+        <span class="text-sm font-light dark:text-white text-gray-600">{date}</span>
+      </div>
+    </div>
+  );
+}
