cefd9c5f6c4e53daea58c7e987f60aada4cef863
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-09-14 13:46:48 +0100

    :art: Update section

--- landing/Member/index.js
+++ landing/Member/index.js
@@ -1,14 +1,14 @@
 export default function Member({ picture, name, role }) {
   return (
-    <div className="w-6/12 sm:w-4/12 md:w-3/12 lg:w-2/12 lg:mb-0 mb-12 mt-12 px-4">
+    <div className="w-12/12 sm:w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/10 2xl:w-2/12 lg:mb-0 mb-12 mt-12 px-4 mx-auto">
       <img
         alt={name}
         src={`img/team/${picture}`}
         className="shadow-lg rounded-full mx-auto max-w-32-px max-h-32-px"
       />
       <div className="p-4 text-center">
-        <p className="text-xl font-bold">{name}</p>
-        <p className="mt-1 text-sm text-purple-600 uppercase font-semibold">
+        <p className="text-4xl sm:text-3xl md:text-2xl lg:text-xl font-bold">{name}</p>
+        <p className="mt-1 text-xl sm:text-lg md:text-md lg:text-sm text-purple-600 uppercase font-semibold">
           {role}
         </p>
       </div>
--- pages/team.js
+++ pages/team.js
@@ -10,26 +10,29 @@ export default function Team() {
         <h1 className="text-4xl sm:text-5xl text-center font-bold m-4">
           Equipa
         </h1>
-        <div className="py-0.5 mx-auto w-6/12 lg:w-3/12 bg-primary h-8/12 text-center text-2xl" />
-        <p className="pt-6 text-xl sm:text-xl font-normal m-4">
-          Adding a "Meet the Team" page or section to your website is an easy,
-          effective way to give your business an accessible face. As one of your
-          most important pages, it gives prospects an idea of who exactly
-          they'll be working with, and shows potential employees that you're
-          proud of the people on your team.
+        <div className="py-0.5 mx-20 mx-auto container w-6/12 lg:w-3/12 bg-primary h-8/12 text-center text-2xl" />
+        <h1 className="mx-20 font-semibold text-primary text-7xl">"</h1>
+        <p className="mx-20 text-black text-3xl leading-relaxed mt-4 mb-4">
+          Nada é impossível. Com a mente singularmente focada e uma resolução sincera - ichinen - o ceú e a terra podem ser movidos tal e qual como um deseja. Não existe nada que não possa ser alcançado.
+        </p>
+        <h1 className="mx-20 font-semibold text-primary text-right text-7xl">
+          "
+        </h1>
+        <p className="mx-20 text-black font-bold text-2xl leading-relaxed mb-4">
+          - Yamamoto Tsunetomo, Hagakure Kikigaki
         </p>
       </div>
       <section className="mb-20 relative">
         <div className="pt-20 container mx-auto px-4">
-          <div className="flex flex-wrap -mt-12">
+          <div className="flex flex-wrap -mt-12 mx-20">
             {team.map((entry) => (
               <Member {...entry} />
             ))}
           </div>
         </div>
       </section>
-      <div className="bg-white">
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <div className="mx-20 bg-white">
+        <div className="max-w-7xl mx-auto py-12 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             <span className="block">Junta-te a nós!</span>
             <span className="block text-primary">
@@ -38,12 +41,12 @@ export default function Team() {
           </h2>
           <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
             <div className="inline-flex rounded-md shadow">
-              <a
-                href=""
-                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-3xl text-white bg-primary hover:bg-purple-600"
+              <button
+                type="button"
+                className="bg-primary text-white rounded-3xl px-4 py-2 transform duration-300 uppercase hover:bg-purple-600"
               >
                 Saber Mais
-              </a>
+              </button>
             </div>
           </div>
         </div>
