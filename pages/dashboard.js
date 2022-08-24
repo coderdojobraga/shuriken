de3b6988a0ce08fa5bb1ccc4f77c110c2615ddf8
Rui Lopes <rui.lilokas2003@hotmail.com>
2022-08-24 22:51:06 +0100

    Fix dashboard (#66)

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,13 @@
-import { Typography, Col, Row } from "antd";
-import { withAuth } from "~/components/Auth";
+import { useEffect, useState } from "react";
+import { Alert, Button, Space, Typography, Col, Row, notification } from "antd";
+import { useAuth, withAuth } from "~/components/Auth";
 import AppLayout from "~/components/layouts/AppLayout";
 import Event from "~/components/Event";
 import Badge from "~/components/Badge";
 import { useBadges } from "~/hooks/badges";
+import * as USER from "~/lib/user";
+import { getEvents, getNinjas } from "~/lib/api";
+import Ninja from "~/components/Ninja";
 import { useEvents } from "~/hooks/events";
 
 import styles from "~/styles/Dashboard.module.css";
@@ -11,8 +15,20 @@ import styles from "~/styles/Dashboard.module.css";
 const { Title } = Typography;
 
 function Dashboard() {
+  const { user } = useAuth();
+  const role = user.role;
+
+  const [ninjas, setNinjas] = useState([]);
   const { data: events, isLoading: isLoadingEvents } = useEvents();
   const { data: badges, isLoading: isLoadingBadges } = useBadges();
+
+  useEffect(() => {
+    if (role === USER.ROLES.GUARDIAN) {
+      getNinjas()
+        .then((response) => setNinjas(response.data))
+        .catch((error) => notification["error"](error.data?.errors));
+    }
+  }, []);
 
   return (
     <AppLayout>
@@ -35,15 +51,41 @@ function Dashboard() {
           </Col>
         ))}
       </Row>
-      <Title level={3}>Crachás</Title>
-      <Row className={styles.row} align="top" justify="start" gutter={[16, 16]}>
-        {badges &&
-          badges?.slice(0, 5).map((badge) => (
-            <Col key={badge.id}>
-              <Badge {...badge} loading={isLoadingBadges} />
-            </Col>
-          ))}
-      </Row>
+      {role === USER.ROLES.GUARDIAN ? (
+        <>
+          <Title level={3}>Ninjas</Title>
+          <Row
+            className={styles.row}
+            align="top"
+            justify="start"
+            gutter={[16, 16]}
+          >
+            {ninjas &&
+              ninjas.slice(0, 5).map((ninja) => (
+                <Col key={ninja.id}>
+                  <Ninja {...ninja} />
+                </Col>
+              ))}
+          </Row>
+        </>
+      ) : (
+        <>
+          <Title level={3}>Crachás</Title>
+          <Row
+            className={styles.row}
+            align="top"
+            justify="start"
+            gutter={[16, 16]}
+          >
+            {badges &&
+              badges.slice(0, 5).map((badge) => (
+                <Col key={badge.id}>
+                  <Badge {...badge} loading={isLoadingBadges} />
+                </Col>
+              ))}
+          </Row>
+        </>
+      )}
     </AppLayout>
   );
 }
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,18 +1,12 @@
 import { useEffect, useState } from "react";
-import Link from "next/link";
-import { Avatar, Button, Card, Row, Typography } from "antd";
-import {
-  CalendarOutlined,
-  EditOutlined,
-  PlusOutlined,
-} from "@ant-design/icons";
+import { Button, Card, Row, Typography, Col } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import { withAuth } from "~/components/Auth";
 import AppLayout from "~/components/layouts/AppLayout";
 import LinkTo from "~/components/utils/LinkTo";
 import * as api from "~/lib/api";
-import Belt from "~/components/Belt";
+import Ninja from "~/components/Ninja";
 
-const { Meta } = Card;
 const { Title } = Typography;
 
 function Ninjas() {
@@ -40,34 +34,9 @@ function Ninjas() {
       </Row>
       <Row justify="space-around" gutter={[10, 10]}>
         {ninjas.map((ninja) => (
-          <Card
-            key={ninja.id}
-            size="large"
-            style={{ width: 300, marginTop: 16 }}
-            actions={[
-              <Link key={`link ${ninja.id}`} href={`/ninjas/edit/${ninja.id}`}>
-                <a>
-                  <EditOutlined key="edit" />
-                </a>
-              </Link>,
-              <Link
-                key={`Sessions ${ninja.id}`}
-                href={`/lectures/ninja/${ninja.id}`}
-              >
-                <a>
-                  <CalendarOutlined key="sessions" />
-                </a>
-              </Link>,
-            ]}
-          >
-            <LinkTo href={`/profile/ninja/${ninja.id}`}>
-              <Meta
-                avatar={<Avatar src={ninja.photo} />}
-                title={`${ninja.first_name} ${ninja.last_name}`}
-              />
-              <Belt belt={ninja.belt} />
-            </LinkTo>
-          </Card>
+          <Col key={ninja.id}>
+            <Ninja {...ninja} />
+          </Col>
         ))}
       </Row>
     </AppLayout>
