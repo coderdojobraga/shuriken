9a33c3f5dda94e3f2dd1e5e18a5b0c500b04c2e2
Rui Lopes <rui.lilokas2003@hotmail.com>
2022-08-24 12:28:29 +0100

    Create event page (#62)

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,66 +1,46 @@
-import { useEffect, useState } from "react";
-import { Alert, Button, Space, Typography, Col, Row } from "antd";
+import { Typography, Col, Row } from "antd";
 import { withAuth } from "~/components/Auth";
 import AppLayout from "~/components/layouts/AppLayout";
 import Event from "~/components/Event";
 import Badge from "~/components/Badge";
 import { useBadges } from "~/hooks/badges";
-import * as api from "~/lib/api";
+import { useEvents } from "~/hooks/events";
 
 import styles from "~/styles/Dashboard.module.css";
 
-const { Title, Paragraph } = Typography;
+const { Title } = Typography;
 
 function Dashboard() {
-  const [events, setEvents] = useState([]);
+  const { data: events, isLoading: isLoadingEvents } = useEvents();
   const { data: badges, isLoading: isLoadingBadges } = useBadges();
-
-  useEffect(() => {
-    api
-      .getEvents()
-      .then((response) => setEvents(response.data))
-      .catch(() => {});
-  }, []);
 
   return (
     <AppLayout>
       <Title level={2}>Painel Principal</Title>
-      <Paragraph>
-        <Alert
-          message="Inscrições Abertas"
-          description={
-            <Space direction="vertical">
-              As inscrições para a sessão de 10 de junho de 2021 estão abertas.
-              <Button size="small" type="primary">
-                Inscrever
-              </Button>
-            </Space>
-          }
-          closable
-          type="info"
-          showIcon
-        />
-      </Paragraph>
       <Title level={3}>Próximo Evento</Title>
       <Row className={styles.row} align="top" justify="space-between">
-        {events.length > 0 ? (
-          <Event event={events[0]} collapsed={false} />
+        {events?.length > 0 ? (
+          <Event
+            event={events[0]}
+            collapsed={false}
+            loading={isLoadingEvents}
+          />
         ) : null}
       </Row>
       <Title level={3}>Eventos</Title>
       <Row className={styles.row} align="top" justify="start" gutter={[16, 16]}>
-        {events.slice(0, 3).map((event) => (
+        {events?.slice(0, 3).map((event) => (
           <Col key={event.id}>
-            <Event event={event} />
+            <Event event={event} loading={isLoadingEvents} />
           </Col>
         ))}
       </Row>
       <Title level={3}>Crachás</Title>
       <Row className={styles.row} align="top" justify="start" gutter={[16, 16]}>
         {badges &&
-          badges.slice(0, 5).map((badge) => (
+          badges?.slice(0, 5).map((badge) => (
             <Col key={badge.id}>
-              <Badge {...badge} />
+              <Badge {...badge} loading={isLoadingBadges} />
             </Col>
           ))}
       </Row>
--- pages/events.js
+++ pages/events.js
@@ -1,31 +1,23 @@
-import { useEffect, useState } from "react";
 import { Typography, Col, Row } from "antd";
 import AppLayout from "~/components/layouts/AppLayout";
 import { withAuth } from "~/components/Auth";
 import Event from "~/components/Event";
-import * as api from "~/lib/api";
+import { useEvents } from "~/hooks/events";
 
 import styles from "~/styles/Dashboard.module.css";
 
 const { Title } = Typography;
 
 function Events() {
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    api
-      .getEvents()
-      .then((response) => setEvents(response.data))
-      .catch(() => {});
-  }, []);
+  const { data: events, isLoading } = useEvents();
 
   return (
     <AppLayout>
       <Title level={2}>Eventos</Title>
       <Row className={styles.row} align="top" justify="start" gutter={[16, 16]}>
-        {events.map((info) => (
+        {events?.map((info) => (
           <Col key={info.id}>
-            <Event event={info} />
+            <Event event={info} loading={isLoading} />
           </Col>
         ))}
       </Row>
