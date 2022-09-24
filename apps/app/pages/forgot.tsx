8e74c662de8c2731b6a5e5b6129893861a186e67
Rui Oliveira <rpoliveira02@gmail.com>
2022-09-24 20:36:01 +0100

    Forgot your password page

---
+++ apps/app/pages/forgot.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { withoutAuth } from "~/components/Auth";
+import AuthenticationLayout from "~/layouts/AuthenticationLayout";
+import { Button, Form, Input, Typography } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+import Visibility from "~/components/Visibility";
+
+interface IFormFields {
+  email: string;
+}
+
+const Forgot = () => {
+  const { Title, Text } = Typography;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [requestSent, setRequestSent] = useState<boolean>(false);
+
+  const submit = ({ email }: IFormFields) => {
+    setRequestSent(true);
+  };
+
+  return (
+    <AuthenticationLayout>
+      <Visibility visible={!requestSent}>
+        <Form name="login" onFinish={submit}>
+          <Title>Recuperar Palavra Passe</Title>
+
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                type: "email",
+                required: true,
+              },
+            ]}
+          >
+            <Input prefix={<MailOutlined />} placeholder="Email" type="email" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" loading={isLoading}>
+              Recuperar
+            </Button>
+          </Form.Item>
+        </Form>
+      </Visibility>
+
+      <Visibility visible={requestSent}>
+        <Title>Verifique a sua caixa de correio.</Title>
+
+        <Text>
+          Enviámos um email com o link para alterar as suas credenciais.
+        </Text>
+      </Visibility>
+    </AuthenticationLayout>
+  );
+};
+
+export default withoutAuth(Forgot);
