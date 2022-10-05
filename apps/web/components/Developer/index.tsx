726be14dddc0fa63861d9d8ab08f232099796fa3
Rui Oliveira <rpoliveira02@gmail.com>
2022-10-05 15:44:29 +0100

    Links

--- apps/web/components/Developer/index.tsx
+++ apps/web/components/Developer/index.tsx
@@ -40,14 +40,15 @@ export default function Developer() {
             >
               Bokken
             </a>
+
             <a
-              href="https://github.com/coderdojobraga"
+              href="https://forms.gle/2e3DUeorMrxYdANz7"
               className="mt-4 mr-1 mb-1 
                                        inline-block rounded px-6 py-4 text-sm
                                        font-bold uppercase text-white outline-none
                                        hover:text-purple-600"
             >
-              Saber Mais
+              Quero contribuir
             </a>
           </div>
           <div className="relative mr-auto ml-auto w-6/12 pl-80 pt-20">
