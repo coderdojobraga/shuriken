2902ede5b647039d6c79e09ab1d5d4a5e5a099f2
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-11-04 23:53:24 +0000

    Fix Belt component

--- apps/app/components/Belt/index.js
+++ apps/app/components/Belt/index.js
@@ -1,16 +1,12 @@
 import { Tag } from "antd";
-import { EBelt } from "bokkenjs";
 import { BELT_PT } from "~/lib/belt";
 
 import styles from "./style.module.css";
 
 export default function Belt({ belt }) {
   return (
-    <Tag
-      className={!belt ? styles.nobelt : styles.capitalize}
-      color={(belt !== EBelt.White && belt !== EBelt.Missing) || null}
-    >
-      {BELT_PT[belt]}
+    <Tag className={!belt ? styles.nobelt : styles.capitalize} color={belt}>
+      {!belt ? BELT_PT[""] : BELT_PT[belt]}
     </Tag>
   );
 }
