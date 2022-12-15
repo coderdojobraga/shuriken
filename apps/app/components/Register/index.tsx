9bdee41e0c93617f3b251774dd06e1135bdcd665
Mário Rodrigues <93675410+MarioRodrigues10@users.noreply.github.com>
2022-12-15 19:40:11 +0000

    Add T-shirt size  (#120)
    * add tshirt size to regist form
    
    * change to portuguese name
    
    * change size to portuguese
    
    * run formatter

--- apps/app/components/Register/index.tsx
+++ apps/app/components/Register/index.tsx
@@ -176,6 +176,16 @@ function Register({ cities }: any) {
               </Form.Item>
             )}
 
+            <Form.Item name="user[t_shirt]" label="Tamanho da T-Shirt">
+              <Select defaultValue="medium">
+                <Option value="extra_small">Muito Pequeno (XS)</Option>
+                <Option value="small">Pequeno (S)</Option>
+                <Option value="medium">Médio (M)</Option>
+                <Option value="large">Grande (L)</Option>
+                <Option value="extra_large">Muito Grande (XL)</Option>
+              </Select>
+            </Form.Item>
+
             <Form.Item
               name="user[photo]"
               label="Foto de perfil"
