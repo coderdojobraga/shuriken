b606700e2166fa67cfab096847bfe0e5f2ace92f
Júlio <94169007+JulioJPinto@users.noreply.github.com>
2023-03-07 19:51:55 +0000

    Add Hall of Fame Page (#153)
    * feat: add hall of fame page
    
    * add champions component
    
    * fix: change hall of fame text
    
    * fix: change champion to organizer
    
    * feat: add hall of fame page
    
    * fix: take out all Champions

--- packages/ui/components/landing/Header/config.ts
+++ packages/ui/components/landing/Header/config.ts
@@ -3,5 +3,6 @@ export const MENU_ENTRIES = [
   { key: "/web/recruitment", text: "Recrutamento" },
   //   { path: "/projects", text: "Projetos" },
   { key: "/web/team", text: "Equipa" },
+  { key: "/web/hall-of-fame", text: "Hall of Fame" },
   { key: "/blog", text: "Blog" },
 ];
