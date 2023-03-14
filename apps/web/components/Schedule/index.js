61d1755c4f8eb46c36b10a1e223fd41983e45a32
pedrofp4444 <pfpereira2004@gmail.com>
2023-03-14 02:03:33 +0000

    Add first day

--- apps/web/components/Schedule/index.js
+++ apps/web/components/Schedule/index.js
@@ -1,77 +1,32 @@
-export default function Schedule() {
+export default function Schedule({ tittle, details, description }) {
   return (
-    <div className="p-6 flex flex-container justify justify-center">
+    <div className="flex-container justify flex pl-10 xl:justify-center">
       <ol className="relative border-l border-gray-200 dark:border-gray-700">
         <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+          <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-100 ring-8 ring-white dark:bg-purple-900 dark:ring-gray-900">
+            <svg
+              aria-hidden="true"
+              className="h-3 w-3 text-purple-800 dark:text-purple-300"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </span>
-          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white"> Mini sessão de abertura</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">10:00 - 10:30 || Júlio Pinto - Organizador DojoCon</time>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>        </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-          </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Apresentação CoderDojo Braga e Projetos</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">10:30 - 11:30 || Champion, Co-Champion e Mentores </time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-        </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-          </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Coffee Break</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">11:30 - 12:00</time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-        </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-          </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Sumo Hacking Challenge</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">12:00 - 13:00 || Rui Oliveira, Rui Lopes e Tiago Bacelar</time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-        </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-          </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Almoço</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">13:00 - 15:00</time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-        </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-          </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Talk JNO</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">15:00 - 16:00 || JNO</time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-        </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-          </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Quiz</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">16:00 - 17:00 || Jéssica Fernandes e Martim Ferreira</time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-        </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-          </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Coffee Break</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">17:00 - 17:30</time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-        </li>
-        <li className="ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
-            <svg aria-hidden="true" className="w-3 h-3 text-purple-800 dark:text-purple-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-          </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Tertúlia de Champions</h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">17:30 - 19:00 || Ex-Champions</time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400 sm:block hidden">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            {tittle}
+          </h3>
+          <time className="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            {details}
+          </time>
+          <p className="hidden text-base font-normal text-gray-500 dark:text-gray-400 sm:block">
+            {description}
+          </p>
         </li>
       </ol>
     </div>
--- apps/web/components/Speaker/index.js
+++ apps/web/components/Speaker/index.js
@@ -1,10 +1,10 @@
 export default function Speaker({ picture, name, role }) {
   return (
-    <div className="w-12/12 xl:w-2/10 mx-auto mb-12 mt-12 px-4 sm:w-6/12 md:w-4/12 lg:mb-0 lg:w-4/12 2xl:w-4/12">
+    <div className="w-12/12 mx-auto mb-12 mt-12 px-4 sm:w-6/12 md:w-4/12 lg:mb-0 lg:w-4/12 xl:w-3/12 2xl:w-4/12">
       <img
         alt={name}
         src={`img/team/${picture}`}
-        className="mx-auto h-56 w-56 rounded-full object-cover shadow-lg md:h-48 md:w-48"
+        className="mx-auto h-52 w-52 rounded-full object-cover shadow-lg md:h-32 md:w-32"
       />
       <div className="p-4 text-center">
         <p className="text-3xl font-bold md:text-2xl lg:text-xl">{name}</p>
