9fa0cb6f278ad00908063afc8612f1cabdaa6b06
pedrofp4444 <pfpereira2004@gmail.com>
2023-03-15 09:38:31 +0000

    Fix minor bug

--- apps/maintenance/pages/_document.tsx
+++ apps/maintenance/pages/_document.tsx
@@ -3,10 +3,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-       <title>CoderDojo Braga</title>
+      <Head />
+      <Head>
+        <title>CoderDojo Braga</title>
       </Head>
       <body>
+        <Main />
         <NextScript />
       </body>
     </Html>
--- apps/maintenance/pages/index.tsx
+++ apps/maintenance/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import React from "react";
 
 const MaintenancePage = () => {
   return (
