e39109cfb7a78456eb15fcf0eddda7c4481635ab
Rui Oliveira <rpoliveira02@gmail.com>
2023-05-14 11:08:21 +0100

    New image and texts

--- apps/web/pages/codercamp.tsx
+++ apps/web/pages/codercamp.tsx
@@ -20,7 +20,7 @@ const CoderCamp = () => (
     </div>
     <section className="overflow-hidden bg-white">
       <div className="container mx-auto">
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap items-center justify-center">
           <div className="ml-auto mr-auto mt-20 w-full px-12 md:w-5/12 md:px-4">
             <div className="bg-primary mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full p-3 text-center text-white shadow-lg">
               <ScheduleOutlined style={{ fontSize: "150%" }} />
@@ -29,11 +29,11 @@ const CoderCamp = () => (
               O que é o Coder Camp?
             </h3>
             <p className="mt-4 mb-4 text-lg font-light leading-relaxed text-black">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              vulputate mauris at ornare faucibus. Etiam suscipit condimentum
-              nisi id ornare. Sed vehicula maximus est vel vestibulum. Nulla
-              ornare elit a urna cursus, sed finibus nunc mollis. Duis et
-              elementum leo, sit amet congue felis.
+              O Coder Camp, organizado pelo CoderDojo Braga, é uma semana
+              dedicada a atividades lúdicas no âmbito da programação, realizada
+              entre os dias 3 a 7 de julho. Esta iniciativa promove a
+              aprendizagem da programação num contexto informal e de convívio,
+              na companhia de outros Ninjas e dos nossos Mentores.
             </p>
             <a
               href=""
@@ -47,40 +47,19 @@ const CoderCamp = () => (
           </div>
           <div className="mr-auto ml-auto mt-20 flex w-full items-center justify-center px-4 sm:w-3/4 md:w-6/12">
             <Image
-              alt="Mentores do Dojo"
-              src="/img/mentor.jpg"
-              width={1944}
-              height={1296}
+              alt="Coder Camp"
+              src="/img/codercamp.png"
+              width={789}
+              height={789}
               className="rounded-2xl shadow-2xl"
             />
-          </div>
-        </div>
-      </div>
-
-      <div className="mx-auto mt-12 mb-20">
-        <div className="relative z-10 flex flex-wrap justify-center bg-white px-12">
-          <div className="w-full lg:w-8/12">
-            <h2 className="text-primary text-7xl font-semibold">&quot;</h2>
-            <p className="mt-4 mb-4 text-2xl leading-relaxed text-black">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              vulputate mauris at ornare faucibus. Etiam suscipit condimentum
-              nisi id ornare. Sed vehicula maximus est vel vestibulum. Nulla
-              ornare elit a urna cursus, sed finibus nunc mollis. Duis et
-              elementum leo, sit amet congue felis.
-            </p>
-            <h2 className="text-primary text-right text-7xl font-semibold">
-              &quot;
-            </h2>
-            <p className="mb-4 text-2xl leading-relaxed text-black">
-              - Jéssica Fernandes, Champion
-            </p>
           </div>
         </div>
       </div>
     </section>
     <section className="bg-primary mt-12 overflow-hidden">
       <div className="container mx-auto mb-20">
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap items-center justify-center">
           <div className="ml-auto mr-auto mt-20 w-full px-12 md:w-5/12 md:px-4">
             <div className="text-primary mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
               <MessageFilled style={{ fontSize: "150%" }} />
@@ -89,11 +68,10 @@ const CoderCamp = () => (
               O que vai acontecer no Coder Camp?
             </h3>
             <p className="mt-4 mb-4 text-lg font-light leading-relaxed text-white">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              vulputate mauris at ornare faucibus. Etiam suscipit condimentum
-              nisi id ornare. Sed vehicula maximus est vel vestibulum. Nulla
-              ornare elit a urna cursus, sed finibus nunc mollis. Duis et
-              elementum leo, sit amet congue felis.
+              Vamos ter sessões dedicadas à programação em Python e Scratch,
+              assim como diversos jogos, atividades de convívio e robôs! O
+              objetivo é criar e desenvolver projetos num ambiente descontraído
+              e promover a interação entre todos os participantes.
             </p>
           </div>
           <div className="relative mr-auto ml-auto w-6/12 pl-80 pt-20">
@@ -109,7 +87,7 @@ const CoderCamp = () => (
     </section>
     <section className="bg-dark overflow-hidden">
       <div className="container mx-auto mb-20 ">
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap items-center justify-center">
           <div className="ml-auto mr-auto mt-20 w-full px-12 md:w-5/12 md:px-4">
             <div className="text-dark mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
               <ScheduleOutlined style={{ fontSize: "150%" }} />
@@ -118,14 +96,8 @@ const CoderCamp = () => (
               Horário
             </h3>
             <p className="mt-4 mb-4 text-lg font-light leading-relaxed text-white">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque sodales faucibus quam, eu lobortis enim efficitur
-              dictum. Aenean pretium bibendum turpis id posuere. Fusce ut mi nec
-              dolor posuere convallis nec venenatis massa. Duis libero dolor,
-              pretium sed ornare a, interdum eget enim. Aliquam nec ultrices
-              quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Integer a euismod justo, a consequat lectus. Curabitur aliquet
-              malesuada lacus.
+              A equipa por detrás do Coder Camp preparou as melhores atividades
+              para uma semana recheada de diversão!
             </p>
           </div>
           <div className="relative mr-auto ml-auto w-6/12 sm:pl-80 sm:pt-20">
