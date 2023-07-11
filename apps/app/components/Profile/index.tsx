d82eee62c067304a67574d4ce758c1dc5ff806d9
Afonsos4ntos <afonso.ds33@gmail.com>
2023-07-11 10:54:36 +0100

    correct some erros

--- apps/app/components/Profile/index.tsx
+++ apps/app/components/Profile/index.tsx
@@ -6,15 +6,10 @@ import {
   Row,
   Space,
   Tabs,
-  Tag,
   Timeline,
   Typography,
 } from "antd";
-import {
-  ClockCircleOutlined,
-  FontSizeOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { ClockCircleOutlined, UserOutlined } from "@ant-design/icons";
 
 import moment from "moment";
 import "moment/locale/pt";
@@ -24,19 +19,13 @@ import Document from "~/components/Document";
 import * as api from "bokkenjs";
 import * as socials from "~/lib/social";
 import { notifyError, notifyInfo } from "~/components/Notification";
-import styles from "./style.module.css";
 import { EUser, getNinjasAsAdmin, updateGuardianAsAdmin } from "bokkenjs";
-
-import { BsFileEarmarkPersonFill } from "react-icons/bs";
 import Link from "next/link";
-
-import { getIcon, getImg } from "~/lib/utils";
-import Guardian from "pages/admin/guardians";
-import { icons } from "react-icons";
+import { getIcon } from "~/lib/utils";
 
 const { TabPane } = Tabs;
 
-const { Title, Text } = Typography;
+const { Title } = Typography;
 
 interface Props {
   id: string;
@@ -49,8 +38,7 @@ function Profile({ id, role }: Props) {
   const [projects, setProjects] = useState<any[]>([]);
   const [skills, setSkills] = useState<any[]>([]);
   const [date, setDate] = useState<string>("");
-  const [guardian, setGuardians] = useState<any[]>([]);
-  const [ninja, setNinja] = useState<any[]>([]);
+  const [guardian, setGuardians] = useState<any | null>([]);
 
   useEffect(() => {
     const fetchUserByRole = async () => {
@@ -135,8 +123,7 @@ function Profile({ id, role }: Props) {
       api
         .getGuardian(info.guardian_id)
         .then((response: any) => setGuardians(response.data))
-        .catch((error: any) => {
-});
+        .catch((error: any) => {});
     }
   }, [info.guardian_id, role]);
   console.log(badges);
@@ -211,7 +198,7 @@ function Profile({ id, role }: Props) {
                     display: "inline-block",
                     fontSize: 20,
                     marginRight: "6px",
-                    color:"#424549"
+                    color: "#424549",
                   }}
                   key={s.id}
                 >
@@ -278,122 +265,3 @@ function Profile({ id, role }: Props) {
 }
 
 export default Profile;
-
-{
-  /* <Row justify="center" align="middle">
-<Space size="large">
-  <Avatar
-    size={{
-      xs: 100,
-      sm: 200,
-      md: 200,
-      lg: 200,
-      xl: 200,
-      xxl: 200,
-    }}
-    src={info?.photo}
-    icon={<UserOutlined />}
-  />
-  <Row justify="center" align="middle">
-    <Col span={24}>
-      <Title level={2}>
-        {info.first_name} {info.last_name}
-      </Title>
-    </Col>
-    <Col span={24}>
-      <Title className={styles.capitalize} level={4}>
-        <BsFileEarmarkPersonFill />{role}
-      </Title>
-    </Col>
-    <Col span={24}>
-      <Title level={5}>Conta criada em: {date}</Title>
-    </Col>
-
-    {"belt" in info && (
-      <Col span={24}>
-        <Belt belt={info.belt} />
-      </Col>
-    )}
-
-    <Col span={24}>
-      <Space style={{ fontSize: 20 }}>
-        {info?.socials?.map((social: any) =>
-          social?.name == "discord" || social?.name == "slack" ? (
-            <a title={social.username}>
-              {socials.ICONS[social.name as keyof typeof socials.URLS]}
-            </a>
-          ) : (
-            <a
-              key={social.id}
-              target="_blank"
-              rel="noreferrer"
-              href={`${
-                socials.URLS[social.name as keyof typeof socials.URLS]
-              }/${social.username}`}
-            >
-              {socials.ICONS[social.name as keyof typeof socials.URLS]}
-            </a>
-          )
-        )}
-      </Space>
-    </Col>
-
-    <Col span={24}>
-      {skills.map((s) => (
-        <Tag key={s.id}>
-          {getIcon(s.name)} {s.name}
-        </Tag>
-      ))}
-    </Col>
-  </Row>
-</Space>
-</Row>
-<Tabs defaultActiveKey="1" centered>
-<TabPane tab="Eventos" key="1">
-  <Timeline mode="alternate">
-    <Timeline.Item dot={<ClockCircleOutlined />}>
-      Registou-se na plataforma{" "}
-      {moment(info.since).locale("pt").fromNow()}
-    </Timeline.Item>
-  </Timeline>
-</TabPane>
-{role === EUser.Ninja && (
-  <TabPane tab="Crachás" key="2">
-    {badges.length == 0 ? (
-      <Empty
-        description="Sem Crachás"
-        image={Empty.PRESENTED_IMAGE_SIMPLE}
-      />
-    ) : (
-      <Row justify="start" align="middle">
-        {badges.map((badge) => (
-          <Col key={badge.id} {...{ xs: 24, md: 12, xl: 8, xxl: 6 }}>
-            <Space>
-              <Badge {...badge} />
-            </Space>
-          </Col>
-        ))}
-      </Row>
-    )}
-  </TabPane>
-)}
-<TabPane tab="Projetos" key="3">
-  {projects.length == 0 ? (
-    <Empty
-      description="Sem Projetos"
-      image={Empty.PRESENTED_IMAGE_SIMPLE}
-    />
-  ) : (
-    <Row justify="start" align="middle">
-      {projects.map((project) => (
-        <Col key={project.id} {...{ xs: 24, md: 12, xl: 8, xxl: 6 }}>
-          <Space>
-            <Document {...project} />
-          </Space>
-        </Col>
-      ))}
-    </Row>
-  )}
-</TabPane>
-</Tabs> */
-}
