4dc997737ee49f23859186406d0599b87f17549e
Afonso Santos <113265283+Afonso-santos@users.noreply.github.com>
2023-10-11 15:47:51 +0100

    fix: white belt style (#200)

--- apps/app/components/Belt/index.js
+++ apps/app/components/Belt/index.js
@@ -4,8 +4,14 @@ import { BELT_PT } from "~/lib/belt";
 import styles from "./style.module.css";
 
 export default function Belt({ belt }) {
+  const isWhite = belt === "white";
+
   return (
-    <Tag className={!belt ? styles.nobelt : styles.capitalize} color={belt}>
+    <Tag
+      className={!belt ? styles.nobelt : styles.capitalize}
+      color={belt}
+      style={isWhite ? { border: "1px solid grey", color: "grey" } : {}}
+    >
       {!belt ? BELT_PT[""] : BELT_PT[belt]}
     </Tag>
   );
